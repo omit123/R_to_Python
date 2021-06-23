3fae244dd831e725a7da40977b7759f0273d7bd6
zs673 <zs673@york.ac.uk>
2021-06-23 15:17:48 +0100

    better x&y lables and ticklables

--- varying_JNE_FP.py
+++ varying_JNE_FP.py
@@ -22,7 +22,6 @@ colors = [
 ]
 ##############################################################################
 
-
 ######################### Gloabl Data Process parameters #########################
 # The input raw data file
 filename = ["summary_results_compute1_for_quick.csv"]
@@ -36,11 +35,12 @@ defaultBaseValue = [2, 0.5, 0.0001, 80]
 # Target Columns: columns that provide data
 targetCols = [["JNE", "TOTAL_LO_CRIT_JOBS"], ["NiH", "TOTAL_HI_CRIT_JOBS"],
               ["TiH", "SIMULATION_DURATION"]]
+
 ##################################################################################
 
 
 # plot line chart for the collected data
-def plotData(datatable, xaxis, labels, name):
+def plotData(datatable, xaxis, ymax, labels, xaxisName, yaxisName, name):
     # All markers available
 
     # plot the figure
@@ -61,13 +61,17 @@ def plotData(datatable, xaxis, labels, name):
                  linewidth=1.0,
                  markersize=3.0)
 
-    # set x,y scale to log
-    plt.yscale('log')
-    plt.xscale('log')
+    # set the same ylim for a setting
+    plt.ylim(-ymax / 10, ymax)
+
+    # set x scale to log if it is FP
+    if xaxisName == 'FP':
+        plt.xscale('log')
+        
 
     # add x,y labels
-    plt.xlabel('FP')
-    plt.ylabel('JNE (%)')
+    plt.xlabel(xaxisName)
+    plt.ylabel(yaxisName + ' (%)')
 
     # add legend
     plt.legend(loc=2, prop={'size': 5})
@@ -86,9 +90,9 @@ def processData(dataframe, policy, cHITocLO, percentHI, failureProb, util,
     for colIndex in targetCols:
         dataCollected.append(
             dataframe[(dataframe["POLICY"] == policy)
-                      & (dataframe["FAILUREPROB(FP)"] == failureProb) &
+                      & (dataframe["CHITOCLO(CF)"] == cHITocLO) &
                       (dataframe["PERCENTHI(CP)"] == percentHI) &
-                      (dataframe["CHITOCLO(CF)"] == cHITocLO) &
+                      (dataframe["FAILUREPROB(FP)"] == failureProb) &
                       (dataframe["UTIL"] == util) &
                       (dataframe["PERIODS"] == period)][colIndex])
 
@@ -109,14 +113,18 @@ def collectData(dataframe, policies, targets, baseData, baseIndex, baseDefault,
         for index in range(len(baseData)):
             # get data for both non harmonic and harmonic periods
             row.append(
-                processData(dataframe=dataframe,
-                            policy=policies[policyIndex],
-                            cHITocLO=baseData[index] if baseIndex == 0 else baseDefault[0],
-                            percentHI=baseData[index] if baseIndex == 1 else baseDefault[1],
-                            failureProb=baseData[index] if baseIndex == 2 else baseDefault[2],
-                            util=baseData[index] if baseIndex == 3 else baseDefault[3],
-                            period=period,
-                            targetCols=targets))
+                processData(
+                    dataframe=dataframe,
+                    policy=policies[policyIndex],
+                    cHITocLO=baseData[index]
+                    if baseIndex == 0 else baseDefault[0],
+                    percentHI=baseData[index]
+                    if baseIndex == 1 else baseDefault[1],
+                    failureProb=baseData[index]
+                    if baseIndex == 2 else baseDefault[2],
+                    util=baseData[index] if baseIndex == 3 else baseDefault[3],
+                    period=period,
+                    targetCols=targets))
 
         datatable.append(row)
 
@@ -124,7 +132,8 @@ def collectData(dataframe, policies, targets, baseData, baseIndex, baseDefault,
 
 
 # Process data for each set of target columns
-def dataProcessAndPlotPerSetting(filename, targets, base, baseIndex, baseDefault, baseAbbr):
+def dataProcessAndPlotPerSetting(filename, targets, base, baseIndex,
+                                 baseDefault, baseAbbr):
 
     print('Processing: ' + targets[0] + '_' + baseAbbr + '_' + "Non_Harmonic")
     print('Processing: ' + targets[0] + '_' + baseAbbr + '_' + "New_Harmonic")
@@ -141,20 +150,33 @@ def dataProcessAndPlotPerSetting(filename, targets, base, baseIndex, baseDefault
     # sort baseData to non-descreasing
     baseData.sort()
 
+    dataForPlot = []
+    yMaxPerPlot = []
     # get data for "NON_HARMONIC" and "NEW_HARMONIC"
     for period in periods:
-        dataForPlot = collectData(dataframe=dataframe,
-                                  policies=policies,
-                                  targets=targets,
-                                  baseData=baseData,
-                                  baseIndex=baseIndex,
-                                  baseDefault=baseDefault,
-                                  period=period)
+        data = collectData(dataframe=dataframe,
+                           policies=policies,
+                           targets=targets,
+                           baseData=baseData,
+                           baseIndex=baseIndex,
+                           baseDefault=baseDefault,
+                           period=period)
+        dataForPlot.append(data)
+        yMaxPerPlot.append(max(map(max, data)))
+
+    ymax = max(yMaxPerPlot)
+
+    for i in range(len(periods)):
         # plot figures
-        plotName = targets[0]+"_for_" + baseAbbr + "_and_"+period+".pdf"
-        plotData(datatable=dataForPlot,
+        plotName = targets[0] + "_for_" + baseAbbr + "_and_" + periods[
+            i] + ".pdf"
+
+        plotData(datatable=dataForPlot[i],
                  xaxis=baseData,
+                 ymax=ymax,
                  labels=policies,
+                 xaxisName=baseAbbr,
+                 yaxisName=targets[0],
                  name=plotName)
 
 
